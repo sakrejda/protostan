70453d3f75fcd9d6286c89c12d36e9da87870cae
Krzysztof Sakrejda <krzysztof@fawkes.io>
2015-12-23 13:04:44 -0500

    Added shim over stan::lang::compile.

---
+++ src/cpp/lang/compiler.hpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <proto/stanc.pb.h>
+#include <stan/lang/compiler.hpp>
+
+namespace stan {
+  namespace proto {
+    stan::proto::StanCompileResponse compile( 
+      stan::proto::StanCompileRequest request) {
+        stan::proto::StanCompileResponse response;
+        std::ostringstream err_stream;
+        std::istringstream stan_stream(request->model_code());
+        std::ostringstream cpp_stream;
+        
+        bool valid_model = stan::lang::compile(&err_stream, 
+          stan_stream, cpp_stream, 
+          request->model_name(), request->model_file_name());
+        response->set_messages(err_stream.str());
+        response->set_cpp_code(cpp_stream.str());
+        if (valid_model) {
+          if (err_stream.tellp() == 0) 
+            response->set_state(stan::proto::StanCompileResponse::SUCCESS);
+          else 
+            response->set_state(stan::proto::StanCompileResponse::WARN);
+        } else {
+          response->set_state(stan::proto::StanCompileResponse::ERROR);
+        }
+          return response;
+      }
+    };    
+
+  }
+}
+
+
+
