f29656a19ca6e1dfaea3ae3e6058f2eba4e09724
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2015-12-23 12:39:40 -0500

    Added test framework and stan repo submodule, compile-run.sh shows how to run a test that tests work...

--- compile-run.sh
+++ compile-run.sh
@@ -22,6 +22,18 @@ cd $ROOT
 bin/protoc --cpp_out=src/cpp ./proto/stanc.proto
 bin/protoc --python_out=src/python ./proto/stanc.proto
 
+# Compile google test library:
+cd $ROOT/lib
+g++ -I stan/lib/stan_math/lib/gtest_1.7.0/include \
+    -I stan/lib/stan_math/lib/gtest_1.7.0/ -c \
+    stan/lib/stan_math/lib/gtest_1.7.0/src/gtest-all.cc
+ar -rv libgtest.a gtest-all.o
 
-
+# Compile/link/run tests:
+cd $ROOT
+g++ -I $ROOT/lib/stan/lib/stan_math/lib/gtest_1.7.0/include \
+    -pthread -o $ROOT/test/unit/gtest-test \
+    $ROOT/src/cpp/test/gtest-test.cpp \
+    $ROOT/lib/libgtest.a
+$ROOT/test/unit/gtest-test
 
