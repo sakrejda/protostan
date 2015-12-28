1c432195f4fa4d5f72baef24d088d1d0a9d10574
Allen Riddell <abr@ariddell.org>
2015-12-27 21:53:20 -0500

    Catch exceptions thrown by stan::lang::compile

--- src/protostan/lang/compiler.hpp
+++ src/protostan/lang/compiler.hpp
@@ -8,16 +8,16 @@ namespace stan {
   namespace proto {
     stan::proto::StanCompileResponse compile(
       const stan::proto::StanCompileRequest& request) {
-        stan::proto::StanCompileResponse response;
-        std::ostringstream err_stream;
-        std::istringstream stan_stream(request.model_code());
-        std::ostringstream cpp_stream;
 
+      stan::proto::StanCompileResponse response;
+      std::ostringstream err_stream;
+      std::istringstream stan_stream(request.model_code());
+      std::ostringstream cpp_stream;
+
+      try {
         bool valid_model = stan::lang::compile(&err_stream,
           stan_stream, cpp_stream,
           request.model_name(), request.model_file_name());
-        response.set_messages(err_stream.str());
-        response.set_cpp_code(cpp_stream.str());
         if (valid_model) {
           if (err_stream.tellp() == 0)
             response.set_state(stan::proto::StanCompileResponse::SUCCESS);
@@ -26,10 +26,12 @@ namespace stan {
         } else {
           response.set_state(stan::proto::StanCompileResponse::ERROR);
         }
-          return response;
+      } catch(const std::exception& e) {
+        response.set_state(stan::proto::StanCompileResponse::ERROR);
       }
+      response.set_messages(err_stream.str());
+      response.set_cpp_code(cpp_stream.str());
+      return response;
+    }
   }
 }
-
-
-
