1eb7504fb0c799774a0b9f0b90bd389b6c3990d2
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2016-01-02 21:07:15 -0500

    TST: additional tests for stan::proto::compile

--- src/test/stanc-test.cpp
+++ src/test/stanc-test.cpp
@@ -10,11 +10,12 @@ TEST(stanc, minimumModelCompile) {
   request.set_model_code("model {}");
   request.set_model_file_name("test.stan");
   response = stan::proto::compile(request);
-  // FIXME: These tests are lame, but that's all that
-  // stan-dev/stan does... (?)
   EXPECT_EQ(stan::proto::StanCompileResponse::SUCCESS, response.state());
-  EXPECT_EQ("", response.messages());
+  EXPECT_STREQ("", response.messages().c_str());
+  // "int main(" should *not* be in the program C++ code
   EXPECT_EQ(std::string::npos, response.cpp_code().find("int main("));
+  // "class test" should be, since the model name is "test"
+  EXPECT_NE(std::string::npos, response.cpp_code().find("class test"));
 }
 
 
@@ -27,8 +28,9 @@ TEST(stanc, invalidModelCompile) {
   request.set_model_file_name("test.stan");
   response = stan::proto::compile(request);
   EXPECT_EQ(stan::proto::StanCompileResponse::ERROR, response.state());
-  EXPECT_EQ("", response.messages());
-  EXPECT_EQ(std::string::npos, response.cpp_code().find("int main("));
+  EXPECT_STREQ("", response.cpp_code().c_str());
+  EXPECT_STRNE("", response.messages().c_str());
+  EXPECT_NE(std::string::npos, response.messages().find("PARSER EXPECTED: <model declaration"));
 }
 
 int main(int argc, char** argv) {
